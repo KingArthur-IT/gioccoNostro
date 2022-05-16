5b2ede7dc41c7b606c0485e0fa9ba064de25cb00
Artem <67517973+KingArthur-IT@users.noreply.github.com>
2022-05-16 20:34:17 +0300

    table component, pagination, label

---
+++ src/components/UIKit/StatusLabel.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="status" :class="`${status.toLowerCase()}`">
+      <span>{{status}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+    props:{
+        status: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+.status{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 10px;
+    border-radius: 10px;
+}
+.confirmed{
+    background: rgba(48, 224, 161, 0.2);
+}
+.confirmed span{
+    color: #30E0A1;
+}
+.output{
+    background: rgba(36, 108, 249, 0.2);
+}
+.output span{
+    color: #246CF9;
+}
+.mistake{
+    background: rgba(250, 34, 86, 0.2);
+}
+.mistake span{
+    color: #FA2256;
+}
+</style>
--- src/views/Transactions.vue
+++ src/views/Transactions.vue
@@ -8,31 +8,161 @@
         <th>Type Game</th>
         <th>Status</th>
     </tr>
-    <tr v-for="item in transactionsData" :key="item.id" class="table__body-row body-text">
+    <tr v-for="item in currentPageArray" :key="item.id" class="table__body-row body-text">
         <td>{{item.id}}</td>
         <td>{{item.date}}</td>
         <td class="type-text">{{item.type}}</td>
         <td>$ {{item.amount}}</td>
         <td>{{item.typeGame}}</td>
-        <td>{{item.status}}</td>
+        <td>
+            <StatusLabel :status="item.status"/>
+        </td>
     </tr>
     </table>
+    <div class="pagination">
+        <PageArrow :isLeft="true" :isEnable="isLeftArrowEnable" @click="prevPage"/>
+        <div class="page-nums-wrapper">
+            <div v-for="i in pageItemsArray" :key="i">
+                <span   :class="{'active-page': i === pageSettings.currentPage}" 
+                        class="page-item"
+                        @click="goToPage(i)"
+                >
+                    {{i}}
+                </span>
+            </div>
+        </div>
+        <PageArrow :isLeft="false" :isEnable="isRightArrowEnable" @click="nextPage"/>
+    </div>
 </template>
 
 <script>
+import { reactive, computed } from 'vue'
+import StatusLabel from '@/components/UIKit/StatusLabel.vue'
+import PageArrow from '@/components/UIKit/PageArrow.vue'
+
 export default {
+    components:{
+        StatusLabel, PageArrow
+    },
     setup(){
         const transactionsData = [
             {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Confirmed'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Output'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Mistake'},
             {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Confirmed'},
             {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Confirmed'},
             {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Confirmed'},
             {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Confirmed'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Output'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Mistake'},
             {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Confirmed'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Confirmed'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Confirmed'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Confirmed'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Output'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Mistake'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Confirmed'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Confirmed'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Confirmed'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Confirmed'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Output'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Mistake'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Confirmed'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Confirmed'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Confirmed'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Confirmed'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Output'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Mistake'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Confirmed'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Confirmed'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Confirmed'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Confirmed'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Output'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Mistake'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Confirmed'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Confirmed'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Confirmed'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Mistake'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Confirmed'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Confirmed'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Confirmed'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Confirmed'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Output'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Mistake'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Confirmed'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Confirmed'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Confirmed'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Confirmed'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Output'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Mistake'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Confirmed'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Confirmed'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Confirmed'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Confirmed'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Output'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Mistake'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Confirmed'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Confirmed'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Confirmed'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Confirmed'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Output'},
+            {id: '000006241252', date: '000006241252', type: 'Incoming', amount: '200', typeGame: '3 x 3', status: 'Mistake'},
         ]
 
+        const pageSettings = reactive({
+            perPage: 10,
+            currentPage: 1,
+        })
+
+        const currentPageArray = computed(() => {
+            return transactionsData.slice((pageSettings.currentPage - 1) * pageSettings.perPage, pageSettings.currentPage * pageSettings.perPage)
+        })
+        const pagesCount = computed(() => {
+            return Math.ceil(transactionsData.length / pageSettings.perPage)
+        })
+        const isLeftArrowEnable = computed(() => {
+            return pageSettings.currentPage > 1
+        })
+        const isRightArrowEnable = computed(() => {
+            return pageSettings.currentPage < pagesCount.value
+        })
+        const isPageItemShow = (index) => {
+            return index === 1 || index === pagesCount.value || Math.abs(index - pageSettings.currentPage) < 2
+        }
+        const pageItemsArray = computed(() => {
+            let hasDots = false,
+                arr = []
+            for(let i = 1; i <= pagesCount.value; i++ ){
+                if (isPageItemShow(i)) 
+                    arr.push(i)
+                else{
+                    if (!hasDots){
+                        arr.push('...');
+                        hasDots = true;
+                    }
+                }
+            }
+            return arr;
+        })
+        const nextPage = () => {
+            if (isRightArrowEnable)
+                pageSettings.currentPage ++;
+        }
+        const prevPage = () => {
+            if (isLeftArrowEnable)
+                pageSettings.currentPage --;
+        }
+        const goToPage = (num) => {
+            if (num !== '...')
+                pageSettings.currentPage = num;
+        }
+
+
         return {
-            transactionsData
+            transactionsData, pageSettings, currentPageArray,
+            pagesCount, isLeftArrowEnable, isRightArrowEnable,
+            isPageItemShow, pageItemsArray,
+            nextPage, prevPage, goToPage
         }
     }
 }
@@ -80,5 +210,27 @@ td:first-child{
 .no-spacing {
   border-spacing:0; 
   border-collapse: collapse;
+}
+.pagination{
+    display: flex;
+    justify-content: flex-end;
+    margin-top: 30px;
+}
+.page-nums-wrapper{
+    display: flex;
+}
+.page-item{
+    color: var(--gray-text-color);
+    cursor: pointer;
+     margin-right: 25px;
+}
+.page-item:first-child{
+    margin-left: 20px;
+}
+.page-item:last-child{
+    margin-right: 20px;
+}
+.active-page{
+    color: var(--primary-text-color)
 }
 </style>
